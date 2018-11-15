da2e7545d6e85d3cd92d36895bfc10558d3487ef
NiklasTR <niklastimon@googlemail.com>
2018-11-14 22:50:07 -0500

    the png export module needs fixing

--- normalize_name.py
+++ normalize_name.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import png
 
 from skimage import data, img_as_float
 from skimage import exposure
@@ -13,7 +14,7 @@ channel = "ch3"
 
 # I select the brightfield images that show my pattern
 # I nest list comprehension, np arrays to create my final list
-brightfield_path = file[np.array([channel in i for i in file])]
+brightfield_path = np.sort(file[np.array([channel in i for i in file])])
 
 # I add the full path
 joined_list = []
@@ -21,11 +22,17 @@ for i in np.ndarray.tolist(brightfield_path):
     joined = os.path.join(dir, i)
     joined_list.append(joined)
 
-#I collapse the list into the standard input format for image collections
-joined_list
-':'.join(joined_list)
+# I create a list of output filenames
+scale_path = []
+for i in range(len(brightfield_path)):
+    scale_path.append('scale_' + brightfield_path[i])
 
-image_coll = io.imread_collection(joined_list)
+
+
+#I collapse the list into the standard input format for image collections
+collapsed_list = ':'.join(joined_list)
+
+image_coll = io.imread_collection(collapsed_list)
 
 ###### dev
 target_std = 0.125
@@ -40,15 +47,7 @@ for i in range(len(image_coll)):
     mat_ms[mat_ms > 1] = 1
     mat_ms[mat_ms < 0] = 0
     #I store
-    image_scaled.append(mat_ms)
-
-
-
-###### dab
-mat_m.std()
-mat_m.mean()
-mat_ms.max()
-joined_list
-len(image_coll)
-type(image_coll[0])
-standardized_data = StandardScaler().fit_transform(data)
+    #image_scaled.append(mat_ms)
+    #I store directly to file using the file names I know already
+    io.imsave(scale_path[i], mat_ms)
+    #png.from_array(mat_ms)
