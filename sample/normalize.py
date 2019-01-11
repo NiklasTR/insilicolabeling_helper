ff9d2464d2441331ab612e9ff44b6c9d06d9a985
NiklasTR <niklastimon@gmail.com>
2019-01-10 19:30:17 -0500

    Update normalize.py

--- sample/normalize.py
+++ sample/normalize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import png
+import sys
 
 from skimage import data, img_as_float
 from skimage import exposure
@@ -12,20 +13,28 @@ import imageio
 import re
 
 # This function is huge but it gets the job done
-def main(channel = "BRIGHTFIELD", file_extension = 'png', target_std = 0.125, target_mean = 0.5 , max_num = 1 , min_num = 0):
+def main(channel = "BRIGHTFIELD", file_extension = 'png', input_extension = 'tiff', target_std = 0.125, target_mean = 0.5 , max_num = 1 , min_num = 0):
 
     #32767.5 # target_mean
     #65535 max_num
 
     dir = sys.argv[1]
+    #for debugging:
+    #dir = '/Users/nrindtor/GitHub/isl_preprocess/local_data/703_cd45/sk2_copy/raw/'
 
     #path = 'local_data/703_cd45/named' #/Users/nrindtor/bucket/flatfield/703__2018-11-07T20_55_16-Measurement_1-sk2-A01-f07-ch2
     #dir = os.path.join(os.getcwd(), path)
-    file = np.array(os.listdir(dir))
+    file_list = os.listdir(dir)
+    # I only keep .tiff images in my list
+    file_list = [i for i in file_list if input_extension in i]
+
+    file = np.array(file_list)
 
     # I select the image_channel images that show my pattern
     # I nest list comprehension, np arrays to create my final list
     image_channel_path = np.sort(file[np.array([channel in i for i in file])])
+    # This
+    #image_channel_path[image_channel_path.defchararray.endswith(file_extension)]
 
     # I add the full path
     joined_list = []
@@ -33,16 +42,21 @@ def main(channel = "BRIGHTFIELD", file_extension = 'png', target_std = 0.125, ta
         joined = os.path.join(dir, i)
         joined_list.append(joined)
 
+
+    #I collapse the list into the standard input format for image collections
+    collapsed_list = ':'.join(joined_list)
+
     # I create a list of output filenames
     scale_path = []
     for i in range(len(image_channel_path)):
         num = re.search(r'(z_depth-)(\d*)', image_channel_path[i]).group(2)
         num = int(num)-1
-        tmp = re.sub(r'depth-\d*', "depth-"+ str(num), image_channel_path[i])
-        scale_path.append(tmp[:-4] + file_extension)
+        new_name = re.sub(r'depth-\d*', "depth-"+ str(num), image_channel_path[i])
+        new_name = new_name[:-4] + file_extension #trimming the file ending
+        joined_new_name = os.path.join(dir, new_name)
+        scale_path.append(joined_new_name)
 
-    #I collapse the list into the standard input format for image collections
-    collapsed_list = ':'.join(joined_list)
+
 
     #I load data
     image_coll = io.imread_collection(collapsed_list)
@@ -63,5 +77,6 @@ def main(channel = "BRIGHTFIELD", file_extension = 'png', target_std = 0.125, ta
         mat_ms = mat_ms.astype(np.uint16)
         #I store
         imageio.imwrite(uri = scale_path[i], im = mat_ms)
+        print("normalized files in: {0}" .format(scale_path[i]))
 
-    print("normalized files in: {0}" .format(dir))
+main()
