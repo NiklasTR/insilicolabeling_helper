bf744e4bbc70aa15b99ea8a8819512918ba1bf8c
NiklasTR <niklastimon@gmail.com>
2019-03-18 11:23:18 -0400

    Update preprocess.py

--- sample/preprocess.py
+++ sample/preprocess.py
@@ -33,7 +33,7 @@ def process_dir_project(path, ch1, ch2, ch3, ch4, avoid_project):         #, ch1
     #renaming
     channel = rename_project.rename_file(path, ch1, ch2, ch3, ch4)
     #normalizing
-    if channel != avoid_project and channel != "processed" and channel != "DPC":
+    if channel != avoid_project and channel != "processed":
         image_channel_path = normalize.identify_files(path, channel = "CCLF")
         scale_path = normalize.create_output_filename(path, image_channel_path)
         normalize.normalize_convert_flourescent(path,image_channel_path, scale_path)
@@ -42,23 +42,22 @@ def process_dir_project(path, ch1, ch2, ch3, ch4, avoid_project):         #, ch1
     else:
         print("Skipped directory or received images that did not match the keep_project pattern")
 
-def __main_manual(path=sys.argv[1], ch1=sys.argv[2], ch2=sys.argv[3], ch3=sys.argv[4], ch4=sys.argv[5], pattern = "Measurement", keep_stack = "BRIGHTFIELD"):
+def __main_manual(path=sys.argv[1], ch1=sys.argv[2], ch2=sys.argv[3], ch3=sys.argv[4], ch4=sys.argv[5], pattern = "Measurement", keep_stack = "BRIGHTFIELD", keep_segmentation = "DPC"):
 #ch1="DPC"
 #ch2="BRIGHTFIELD"
 #ch3="CE"
 #ch4="TMRM"
     # creating list of dirs
-    #path = '/Users/nrindtor/GitHub/isl_preprocess/local_data/test'
+    #path = '/home/ubuntu/bucket/flatfield/000012048903__2019-02-05T20_27_41-Measurement_1/'
     pattern = sys.argv[6]
     dir_list = os.listdir(path)
     dir_list = [i for i in dir_list if pattern in i]
     #I define a dictionary of channels
-    channel_dict = {"ch1" : ch1,
-                    "ch2" : ch2,
-                    "ch3" : ch3,
-                    "ch4" : ch4}
+    channel_dict = {"ch1" : ch1, "ch2" : ch2, "ch3" : ch3,"ch4" : ch4}
     d_inverted={v:k for k,v in channel_dict.items()}
     pattern_stack = d_inverted[keep_stack]
+    # I don't want segmentation data
+    pattern_segmentation = d_inverted[keep_segmentation]
     #I only keep stacks for the directory list and run the loop
     dir_list_stack = [i for i in dir_list if pattern_stack in i]
     for dir in dir_list_stack:
@@ -66,6 +65,7 @@ def __main_manual(path=sys.argv[1], ch1=sys.argv[2], ch2=sys.argv[3], ch3=sys.ar
         process_dir_stack(path_joined, ch1=ch1, ch2=ch2, ch3=ch3, ch4=ch4, keep_stack=keep_stack)
     # I treat everything else as a projection
     dir_list_project = [i for i in dir_list if pattern_stack not in i]
+    dir_list_project = [i for i in dir_list_project if pattern_segmentation not in i]
     for dir in dir_list_project:
         path_joined = os.path.join(path, dir)
         process_dir_project(path_joined, ch1=ch1, ch2=ch2, ch3=ch3, ch4=ch4, avoid_project=keep_stack)
