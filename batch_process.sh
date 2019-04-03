bfeae5543ec0b4c00b7bf917d219800dc68d61de
NiklasTR <niklastimon@gmail.com>
2019-04-03 18:31:46 -0400

    Update batch_process.sh

--- batch_process.sh
+++ batch_process.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-06T20_33_15-Measurement_2/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
-python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-05T20_27_41-Measurement_1/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
-python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-07T20_15_54-Measurement_3/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
-python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-08T17_19_00-Measurement_4/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+#python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-08T17_19_00-Measurement_4/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+#python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012049003__2019-02-01T12_09_07-Measurement_4/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
 
+#!/bin/sh
+python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048703__2019-02-27T19_27_30-Measurement_1/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048703__2019-02-28T19_33_14-Measurement_4/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048703__2019-03-01T17_40_42-Measurement_6/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+
+python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-05T20_27_41-Measurement_1/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-06T20_33_15-Measurement_2/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012048903__2019-02-07T20_15_54-Measurement_3/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
+
+python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012049003__2019-01-28T21_02_07-Measurement_2/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
 python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012049003__2019-01-29T19_11_28-Measurement_3/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
 python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012049003__2019-01-31T18_40_09-Measurement_1/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
-python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012049003__2019-02-01T12_09_07-Measurement_4/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
-python /home/ubuntu/isl_preprocess/sample/preprocess.py /home/ubuntu/bucket/flatfield/000012049003__2019-01-28T21_02_07-Measurement_2/ "DPC" "BRIGHTFIELD" "CE" "TMRM" "Measurement"
