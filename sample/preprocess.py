33b05687eabe5393c58edbd49f894785579eeb62
NiklasTR <niklastimon@gmail.com>
2019-04-18 17:09:06 -0400

    Update preprocess.py

--- sample/preprocess.py
+++ sample/preprocess.py
@@ -92,10 +92,6 @@ if __name__ == '__main__':
     dir_list_project = [i for i in dir_list if pattern_stack not in i]
     dir_list_project = [i for i in dir_list_project if pattern_segmentation not in i]
 
-    def project_helper(dir):
+    with multiprocessing.Pool(number_of_workers) as p:
         print("received input dir")
-        path_joined = os.path.join(path, dir)
-        process_dir_project(path_joined, ch1=ch1, ch2=ch2, ch3=ch3, ch4=ch4, avoid_project=keep_stack)
-        print("processed input dir")
-    #for dir in dir_list_project:
-    #    project_helper(dir)
+        p.map(partial(process_dir_project, path = path, ch1=ch1, ch2=ch2, ch3=ch3, ch4=ch4, avoid_project=keep_stack), dir_list_stack) #(partial(merge_names, b='Sons'), names)
